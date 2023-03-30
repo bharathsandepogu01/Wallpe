44a6e10320d1f251b47c6ddc9eedc31d63c04181
bharathsandepogu01 <bharathsandepogu01@gmail.com>
2023-03-30 21:37:00 +0530

    config: alias import support added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 'off',
+  },
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@configs': './src/configs',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@styles': './src/styles',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+        },
+      },
+    },
+  },
 };
