bf94f397bc0d1a3595c0b7287c29dd5afa7a593b
bharathsandepogu01 <bharathsandepogu01@gmail.com>
2023-04-07 02:22:07 +0530

    feat(dark-mode): added dark mode toggle feature and UI changes in settings screen

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,12 +1,92 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import styles from './styles';
+import React, {useContext} from 'react';
+import {View, ScrollView} from 'react-native';
+import {AppThemeContext} from '@components/AppThemeProvider';
+import PText from '@components/PText';
+import PIcon from '@components/PIcon';
+import PToggle from '@components/PToggle';
+import {AppTheme} from '@components/AppThemeProvider/types';
+import {storeData} from '@utils/asyncStorage';
+import {USER_APP_THEME_PREFERENCE} from '@constants/asyncStorage';
+import DarkModeIcon from '@assets/icons/dark-mode-icon.svg';
+import WifiIcon from '@assets/icons/wifi-icon.svg';
+import DownloadIcon from '@assets/icons/download-icon.svg';
+import MobileScreenIcon from '@assets/icons/mobile-screen-icon.svg';
+import StarIcon from '@assets/icons/star-icon.svg';
+import getSettingsThemeStyles from './styles';
 
 function Settings(): JSX.Element {
+  const {setAppTheme, appTheme, stylesConfig} = useContext(AppThemeContext);
+
+  const storeUserPreferredAppTheme = async (theme: AppTheme) => {
+    await storeData(USER_APP_THEME_PREFERENCE, theme);
+  };
+
+  const styles = getSettingsThemeStyles(stylesConfig);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Settings Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.commonHeaderStyles}>
+        <PText small bold secondaryTextColor>
+          General
+        </PText>
+      </View>
+      <View style={styles.commonHeaderStyles}>
+        <PText small bold secondaryTextColor>
+          Preferences
+        </PText>
+      </View>
+      <View style={styles.commonContentContainer}>
+        <View style={styles.commonIconContainer}>
+          <PIcon icon={DarkModeIcon} />
+          <PText small>Dark Mode</PText>
+        </View>
+        <PToggle
+          enabled={appTheme === 'dark'}
+          enableFn={() => {
+            setAppTheme('dark');
+            storeUserPreferredAppTheme('dark');
+          }}
+          disableFn={() => {
+            setAppTheme('light');
+            storeUserPreferredAppTheme('light');
+          }}
+        />
+      </View>
+      <View style={styles.commonContentContainer}>
+        <View style={styles.commonIconContainer}>
+          <PIcon icon={WifiIcon} />
+          <PText small>Only Download via Wi-Fi</PText>
+        </View>
+        <PToggle />
+      </View>
+      <View style={styles.commonContentContainer}>
+        <View style={styles.commonIconContainer}>
+          <PIcon icon={DownloadIcon} />
+          <PText small>Long Press to Download</PText>
+        </View>
+        <PToggle />
+      </View>
+      <View style={styles.commonHeaderStyles}>
+        <PText small bold secondaryTextColor>
+          About
+        </PText>
+      </View>
+      <View style={styles.commonContentContainer}>
+        <View style={styles.commonIconContainer}>
+          <PIcon icon={StarIcon} />
+          <PText small>Rate App</PText>
+        </View>
+      </View>
+      <View style={styles.commonContentContainer}>
+        <View style={styles.commonIconContainer}>
+          <PIcon icon={MobileScreenIcon} />
+          <PText small>App Version</PText>
+        </View>
+        <PText small tertiaryTextColor>
+          1.2.0
+        </PText>
+      </View>
+    </ScrollView>
   );
 }
 
--- src/screens/Settings/styles.ts
+++ src/screens/Settings/styles.ts
@@ -1,15 +1,30 @@
 import {StyleSheet} from 'react-native';
+import appSpacingConfig from '@styles/spacing';
+import IColorsConfig from '@styles/colors/types';
+import {getDP} from '@styles/common';
 
-export default StyleSheet.create({
-  container: {
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  text: {
-    fontSize: 30,
-    color: 'red',
-    fontFamily: 'MontserratAlternates-Regular',
-  },
-});
+export default function getSettingsThemeStyles(colorsConfig: IColorsConfig) {
+  return StyleSheet.create({
+    commonHeaderStyles: {
+      backgroundColor: colorsConfig.backgroundColorLight,
+      paddingVertical: appSpacingConfig.padding8,
+      paddingHorizontal: appSpacingConfig.padding20,
+    },
+    commonContentContainer: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      paddingHorizontal: appSpacingConfig.padding12,
+      paddingVertical: appSpacingConfig.padding4,
+      gap: 4,
+    },
+    commonIconContainer: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+    },
+  });
+}
