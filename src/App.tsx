88bf4c287803df96c656f5d9b5855e9425d0afe8
bharathsandepogu01 <bharathsandepogu01@gmail.com>
2023-04-07 02:02:00 +0530

    config(apollo-client): apollo client setup for graphql api querying and instance data caching

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigation from '@navigation/index';
-import {ThemeProvider} from '@react-navigation/native';
 import AppThemeProvider from '@components/AppThemeProvider';
+import ApolloClientProvider from '@components/ApolloClientProvider';
 
 function App(): JSX.Element {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <SafeAreaProvider>
         <AppThemeProvider>
-          <Navigation />
+          <ApolloClientProvider>
+            <Navigation />
+          </ApolloClientProvider>
         </AppThemeProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
---
+++ src/components/ApolloClientProvider/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import {BASE_URL} from '@constants/apolloGraphQL';
+import IApolloClientProviderProps from './types';
+
+// Initializing apollo client
+const client = new ApolloClient({
+  uri: BASE_URL,
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          getImages: {
+            // Don't cache separate results based on
+            // any of this field's arguments.
+            keyArgs: false,
+
+            merge(existing, incoming) {
+              // Slicing is necessary because the existing data is
+              // immutable, and frozen in development.
+              const merged = existing ? existing.slice(0) : [];
+
+              for (let i = 0; i < incoming.length; ++i) {
+                merged.push(incoming[i]);
+              }
+
+              return merged;
+            },
+          },
+        },
+      },
+    },
+  }),
+});
+
+export default function ApolloClientProvider(
+  props: IApolloClientProviderProps,
+): JSX.Element {
+  return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
+}
